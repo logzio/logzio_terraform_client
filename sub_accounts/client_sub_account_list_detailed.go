4929d8f537a97e0d2e91056d5c9f7b4dd7b48257
Yogev Mets <yyyogev@gmail.com>
2020-03-19 22:15:22 +0200

    Add subaccounts support (#61)
    * Get sub accounts detailed info
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>
    
    * Structs
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>
    
    * Sub account detailed info type
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>
    
    * parse json
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>
    
    * get detailed info by id
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>
    
    * get detailed subaccounts tests
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>
    
    * dates tests
    
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

---
+++ sub_accounts/client_sub_account_list_detailed.go
@@ -0,0 +1,76 @@
+package sub_accounts
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jonboydell/logzio_client"
+	"github.com/jonboydell/logzio_client/client"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	detailedServiceUrl     string = subAccountServiceEndpoint + "/detailed"
+	detailedServiceMethod  string = http.MethodGet
+	detailedServiceSuccess int    = http.StatusOK
+)
+
+func (c *SubAccountClient) detailedValidateRequest(id int64) (error, bool) {
+	return nil, true
+}
+
+func (c *SubAccountClient) detailedApiRequest(apiToken string) (*http.Request, error) {
+	url := fmt.Sprintf(detailedServiceUrl, c.BaseUrl)
+	req, err := http.NewRequest(detailedServiceMethod, url, nil)
+	logzio_client.AddHttpHeaders(apiToken, req)
+	return req, err
+}
+
+func (c *SubAccountClient) detailedHttpRequest(req *http.Request) ([]map[string]interface{}, error) {
+	httpClient := client.GetHttpClient(req)
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	jsonBytes, err := ioutil.ReadAll(resp.Body)
+	if !logzio_client.CheckValidStatus(resp, []int{detailedServiceSuccess}) {
+		return nil, fmt.Errorf("%d %s", resp.StatusCode, jsonBytes)
+	}
+	var target []map[string]interface{}
+	err = json.Unmarshal(jsonBytes, &target)
+	if err != nil {
+		return nil, err
+	}
+	return target, nil
+}
+
+func (c *SubAccountClient) detailedCheckResponse(response []map[string]interface{}) error {
+	return nil
+}
+
+func (c *SubAccountClient) DetailedSubAccounts() ([]SubAccountDetailed, error) {
+
+	req, _ := c.detailedApiRequest(c.ApiToken)
+
+	target, err := c.detailedHttpRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.detailedCheckResponse(target)
+	if err != nil {
+		return nil, err
+	}
+
+	var subAccountsDetailed []SubAccountDetailed
+	for _, jsonObject := range target {
+		subAccount, err := jsonToDetailedSubAccount(jsonObject)
+		if err != nil {
+			return  nil, err
+		}
+		subAccountsDetailed = append(subAccountsDetailed, *subAccount)
+	}
+
+	return subAccountsDetailed, nil
+}
