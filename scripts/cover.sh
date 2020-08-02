8b58631a926601c5fefc620b70d53fa8aa675d54
yyyogev <yogev.metzuyanim@logz.io>
2020-08-02 11:54:03 +0300

    jonboydell -> logzio
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ -z ${LOGZIO_API_TOKEN} ]; then echo "Must supply a LOGZIO_API_TOKEN env var" && exit 1; fi
-PREFIX=github.com/jonboydell/logzio_client
+PREFIX=github.com/logzio/logzio_terraform_client
 go test ${PREFIX}/alerts ${PREFIX}/client ${PREFIX}/endpoints ${PREFIX}/users -coverprofile=coverage.out && go tool cover -html=coverage.out
