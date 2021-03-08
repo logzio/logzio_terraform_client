77fda9ee28fe5473e51b1f567b72e498cd9f2e62
yyyogev <yogev.metzuyanim@logz.io>
2021-03-08 17:23:37 +0200

    custom endpoint create unit test
    Signed-off-by: yyyogev <yogev.metzuyanim@logz.io>

---
+++ endpoints/endpoints_create_custom_endpoint_test.go
@@ -0,0 +1,51 @@
+package endpoints_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/logzio/logzio_terraform_client/endpoints"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEndpoints_CreateCustomEndpoint(t *testing.T) {
+	underTest, err, teardown := setupEndpointsTest()
+	defer teardown()
+
+	endpointId := int64(1234567)
+
+	mux.HandleFunc("/v1/endpoints/custom", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		assert.Equal(t, http.MethodPost, r.Method)
+		jsonBytes, _ := ioutil.ReadAll(r.Body)
+		var target map[string]interface{}
+		err = json.Unmarshal(jsonBytes, &target)
+		assert.Contains(t, target, "title")
+		assert.Contains(t, target, "description")
+		assert.Contains(t, target, "url")
+		assert.Contains(t, target, "headers")
+		assert.Contains(t, target, "two words")
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, fixture("create_endpoint.json"))
+	})
+
+	if assert.NoError(t, err) {
+		endpoint, err := underTest.CreateEndpoint(endpoints.Endpoint{
+			Title:         "testCreateCustomEndpoint",
+			Method:       "POST",
+			Description:   "my description",
+			Url:          "https://this.is.com/some/other/webhook",
+			EndpointType:  endpoints.EndpointTypeCustom,
+			Headers:      map[string]string{"hello": "there", "header": "two words"},
+			BodyTemplate: map[string]string{"hello": "there", "header": "two"},
+		})
+		assert.NoError(t, err)
+		assert.NotNil(t, endpoint)
+		assert.Equal(t, endpointId, endpoint.Id)
+	}
+}
