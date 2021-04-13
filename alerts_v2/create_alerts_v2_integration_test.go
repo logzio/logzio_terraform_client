55fd22e6929f899152acd87e2b1dc4aeec4589bb
mirii1994 <miri.ignatiev@logz.io>
2021-04-13 18:10:32 +0300

    more create intg tests

--- alerts_v2/create_alerts_v2_integration_test.go
+++ alerts_v2/create_alerts_v2_integration_test.go
@@ -1,7 +1,7 @@
 package alerts_v2_test
 
 import (
-	"fmt"
+	"github.com/logzio/logzio_terraform_client/alerts_v2"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -12,11 +12,11 @@ func TestIntegrationAlertsV2_CreateAlert(t *testing.T) {
 
 	if assert.NoError(t, err) {
 		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2"
 		alert, err := underTest.CreateAlert(createAlert)
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(4 * time.Second)
 		if assert.NoError(t, err) && assert.NotNil(t, alert) {
-			t.Log(fmt.Sprintf("One alert id: %d", alert.AlertId))
 			defer underTest.DeleteAlert(alert.AlertId)
 		}
 	}
@@ -27,12 +27,13 @@ func TestIntegrationAlertsV2_CreateAlertWithFilter(t *testing.T) {
 
 	if assert.NoError(t, err) {
 		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2 with filter"
 		match := map[string]string{"type": "mytype"}
 		must := map[string]interface{}{"match": match}
 		createAlert.SubComponents[0].QueryDefinition.Filters.Bool.Must = append(createAlert.SubComponents[0].QueryDefinition.Filters.Bool.Must, must)
 		alert, err := underTest.CreateAlert(createAlert)
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(4 * time.Second)
 		if assert.NoError(t, err) && assert.NotNil(t, alert) {
 			defer underTest.DeleteAlert(alert.AlertId)
 		}
@@ -44,7 +45,66 @@ func TestIntegrationAlertsV2_CreateAlertInvalidFilter(t *testing.T) {
 
 	if assert.NoError(t, err) {
 		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2 with invalid filter"
 		createAlert.SubComponents[0].QueryDefinition.Filters.Bool.Must = append(createAlert.SubComponents[0].QueryDefinition.Filters.Bool.Must, nil)
+
+		alert, err := underTest.CreateAlert(createAlert)
+		assert.Error(t, err)
+		assert.Nil(t, alert)
+	}
+}
+
+func TestIntegrationAlertsV2_CreateAlertInvalidAggregationType(t *testing.T) {
+	underTest, err := setupAlertsV2IntegrationTest()
+
+	if assert.NoError(t, err) {
+		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2 with invalid agg type"
+		createAlert.SubComponents[0].QueryDefinition.Aggregation.AggregationType = "INVALID"
+
+		alert, err := underTest.CreateAlert(createAlert)
+		assert.Error(t, err)
+		assert.Nil(t, alert)
+	}
+}
+
+func TestIntegrationAlertsV2_CreateAlertInvalidValueAggregationTypeNone(t *testing.T) {
+	underTest, err := setupAlertsV2IntegrationTest()
+
+	if assert.NoError(t, err) {
+		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2 with invalid val agg type none"
+		createAlert.SubComponents[0].QueryDefinition.Aggregation.AggregationType = alerts_v2.AggregationTypeNone
+		createAlert.SubComponents[0].QueryDefinition.Aggregation.FieldToAggregateOn = "hello"
+
+		alert, err := underTest.CreateAlert(createAlert)
+		assert.Error(t, err)
+		assert.Nil(t, alert)
+	}
+}
+
+func TestIntegrationAlertsV2_CreateAlertInvalidValueAggregationTypeCount(t *testing.T) {
+	underTest, err := setupAlertsV2IntegrationTest()
+
+	if assert.NoError(t, err) {
+		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2 with invalid val agg type count"
+		createAlert.SubComponents[0].QueryDefinition.Aggregation.AggregationType = alerts_v2.AggregationTypeCount
+		createAlert.SubComponents[0].QueryDefinition.Aggregation.FieldToAggregateOn = "hello"
+
+		alert, err := underTest.CreateAlert(createAlert)
+		assert.Error(t, err)
+		assert.Nil(t, alert)
+	}
+}
+
+func TestIntegrationAlertsV2_CreateAlertInvalidMail(t *testing.T) {
+	underTest, err := setupAlertsV2IntegrationTest()
+
+	if assert.NoError(t, err) {
+		createAlert := getCreateAlertType()
+		createAlert.Title = "test alerts v2 invalid mail"
+		createAlert.Output.Recipients.Emails = []string{""}
 
 		alert, err := underTest.CreateAlert(createAlert)
 		assert.Error(t, err)
