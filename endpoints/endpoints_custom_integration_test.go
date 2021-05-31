b80b333720b66717f6ad840545a4a7a45fd8bdbf
mirii1994 <miri.ignatiev@logz.io>
2021-05-31 13:18:49 +0300

    add endpoints tests

--- endpoints/endpoints_custom_integration_test.go
+++ endpoints/endpoints_custom_integration_test.go
@@ -79,13 +79,10 @@ func TestIntegrationEndpoints_CustomCreateNoHeader(t *testing.T) {
 			BodyTemplate: map[string]string{"hello": "there", "header": "two"},
 		})
 
-		if endpoint != nil {
+		if assert.NotNil(t, endpoint) && assert.NoError(t, err) {
+			assert.NotEmpty(t, endpoint.Id)
 			defer underTest.DeleteEndpoint(endpoint.Id)
 		}
-
-		assert.NotNil(t, endpoint)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, endpoint.Id)
 	}
 }
 
@@ -101,20 +98,41 @@ func TestIntegrationEndpoints_CustomGetNoHeaders(t *testing.T) {
 			BodyTemplate: map[string]string{"hello": "there", "header": "two"},
 		})
 
-		if endpoint != nil {
+		if assert.NotNil(t, endpoint) && assert.NoError(t, err) {
 			defer underTest.DeleteEndpoint(endpoint.Id)
+			assert.NotEmpty(t, endpoint.Id)
+			time.Sleep(4 * time.Second)
+			getEndpoint, err := underTest.GetEndpoint(endpoint.Id)
+			assert.NotNil(t, endpoint)
+			assert.NoError(t, err)
+			assert.Equal(t, endpoint.Id, getEndpoint.Id)
 		}
+	}
+}
 
-		assert.NotNil(t, endpoint)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, endpoint.Id)
+func TestIntegrationEndpoints_CustomCreateInvalidMethod(t *testing.T) {
+	underTest, err := setupEndpointsIntegrationTest()
+	if assert.NoError(t, err) {
+		endpoint, err := underTest.CreateEndpoint(endpoints.Endpoint{
+			Title:        "testCreateCustomEndpoint",
+			Method:       "INVALID",
+			Description:  "my description",
+			Url:          "https://jsonplaceholder.typicode.com/todos/1",
+			EndpointType: "custom",
+			Headers:      map[string]string{"hello": "there", "header": "two"},
+			BodyTemplate: map[string]string{"hello": "there", "header": "two"},
+		})
 
-		time.Sleep(4 * time.Second)
+		assert.Error(t, err)
+		assert.Nil(t, endpoint)
+	}
+}
 
-		getEndpoint, err := underTest.GetEndpoint(endpoint.Id)
-
-		assert.NotNil(t, endpoint)
-		assert.NoError(t, err)
-		assert.Equal(t, endpoint.Id, getEndpoint.Id)
+func TestIntegrationEndpoints_CustomGetNotExists(t *testing.T) {
+	underTest, err := setupEndpointsIntegrationTest()
+	if assert.NoError(t, err) {
+		endpoint, err := underTest.GetEndpoint(int64(1234567))
+		assert.Error(t, err)
+		assert.Nil(t, endpoint)
 	}
 }
