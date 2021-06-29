491b6383db97c254bfc9d9e1106f2cedfa4460c6
mirii1994 <miri.ignatiev@logz.io>
2021-06-29 15:54:45 +0300

    get test and integration test

---
+++ log_shipping_tokens/get_log_shipping_tokens_integration_test.go
@@ -0,0 +1,32 @@
+package log_shipping_tokens_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestIntegrationLogShippingTokens_GetToken(t *testing.T) {
+	underTest, err := setupLogShippingTokensIntegrationTest()
+	if assert.NoError(t, err) {
+		createToken := getCreateLogShippingToken()
+		token, err := underTest.CreateLogShippingToken(createToken)
+
+		if assert.NoError(t, err) && assert.NotNil(t, token) {
+			time.Sleep(2 * time.Second)
+			defer underTest.DeleteLogShippingToken(token.Id)
+			tokenFromGet, err := underTest.GetLogShippingToken(token.Id)
+			assert.NoError(t, err)
+			assert.NotNil(t, tokenFromGet)
+		}
+	}
+}
+
+func TestIntegrationLogShippingTokens_GetTokenNotExist(t *testing.T) {
+	underTest, err := setupLogShippingTokensIntegrationTest()
+	if assert.NoError(t, err) {
+		tokenFromGet, err := underTest.GetLogShippingToken(int32(123456))
+		assert.Error(t, err)
+		assert.Nil(t, tokenFromGet)
+	}
+}
