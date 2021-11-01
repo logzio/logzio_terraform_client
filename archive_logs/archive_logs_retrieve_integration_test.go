b9be65c89a2d746f72147512fb08e013cc0cadc0
mirii1994 <miri.ignatiev@logz.io>
2021-11-01 16:30:31 +0200

    add tests to archive retrieve

--- archive_logs/archive_logs_retrieve_integration_test.go
+++ archive_logs/archive_logs_retrieve_integration_test.go
@@ -1,30 +1,39 @@
 package archive_logs_test
 
-// TODO
-//import (
-//	"github.com/logzio/logzio_terraform_client/archive_logs"
-//	"github.com/stretchr/testify/assert"
-//	"testing"
-//	"time"
-//)
-//
-//func TestIntegrationArchiveLogs_RetrieveArchiveS3(t *testing.T) {
-//	underTest, err := setupArchiveLogsIntegrationTest()
-//
-//	if assert.NoError(t, err) {
-//		createArchive, err := getCreateOrUpdateArchiveLogs(archive_logs.StorageTypeS3)
-//		if assert.NoError(t, err) {
-//			archive, err := underTest.SetupArchive(createArchive)
-//			if assert.NoError(t, err) && assert.NotNil(t, archive) {
-//				defer underTest.DeleteArchiveLogs(archive.Id)
-//				time.Sleep(2 * time.Second)
-//				getArchive, err := underTest.RetrieveArchiveLogsSetting(archive.Id)
-//				assert.NoError(t, err)
-//				assert.NotNil(t, getArchive)
-//				assert.Equal(t, archive.Id, getArchive.Id)
-//				assert.Equal(t, archive.Settings, getArchive.Settings)
-//			}
-//		}
-//
-//	}
-//}
+import (
+	"github.com/logzio/logzio_terraform_client/archive_logs"
+	"github.com/logzio/logzio_terraform_client/test_utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestIntegrationArchiveLogs_RetrieveArchive(t *testing.T) {
+	underTest, err := setupArchiveLogsIntegrationTest()
+
+	if assert.NoError(t, err) {
+		createArchive, err := test_utils.GetCreateOrUpdateArchiveLogs(archive_logs.StorageTypeS3)
+		if assert.NoError(t, err) {
+			archive, err := underTest.SetupArchive(createArchive)
+			if assert.NoError(t, err) && assert.NotNil(t, archive) {
+				defer underTest.DeleteArchiveLogs(archive.Id)
+				time.Sleep(2 * time.Second)
+				getArchive, err := underTest.RetrieveArchiveLogsSetting(archive.Id)
+				assert.NoError(t, err)
+				assert.NotNil(t, getArchive)
+				assert.Equal(t, archive.Id, getArchive.Id)
+				assert.Equal(t, archive.Settings, getArchive.Settings)
+			}
+		}
+	}
+}
+
+func TestIntegrationArchiveLogs_RetrieveArchiveIdNotFound(t *testing.T) {
+	underTest, err := setupArchiveLogsIntegrationTest()
+
+	if assert.NoError(t, err) {
+		archive, err := underTest.RetrieveArchiveLogsSetting(int32(1234))
+		assert.Error(t, err)
+		assert.Nil(t, archive)
+	}
+}
--- archive_logs/archive_logs_retrieve_test.go
+++ archive_logs/archive_logs_retrieve_test.go
@@ -1,3 +1,73 @@
 package archive_logs_test
 
-//TODO
+import (
+	"fmt"
+	"github.com/logzio/logzio_terraform_client/archive_logs"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"strconv"
+	"testing"
+)
+
+func TestArchiveLogs_RetrieveArchive(t *testing.T) {
+	underTest, teardown, err := setupArchiveLogsTest()
+	defer teardown()
+	assert.NoError(t, err)
+	archiveId := int32(1234)
+
+	mux.HandleFunc(archiveApiBasePath+"/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Contains(t, r.URL.String(), strconv.FormatInt(int64(archiveId), 10))
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, fixture("get_archive.json"))
+	})
+
+	archive, err := underTest.RetrieveArchiveLogsSetting(archiveId)
+	assert.NoError(t, err)
+	assert.NotNil(t, archive)
+	assert.Equal(t, archiveId, archive.Id)
+	assert.Equal(t, archive_logs.StorageTypeBlob, archive.Settings.StorageType)
+	assert.True(t, archive.Settings.Enabled)
+	assert.True(t, archive.Settings.Compressed)
+	assert.Equal(t, "some-tenant-id", archive.Settings.AzureBlobStorageSettings.TenantId)
+	assert.Equal(t, "some-client-id", archive.Settings.AzureBlobStorageSettings.ClientId)
+	assert.Equal(t, "some-client-secret", archive.Settings.AzureBlobStorageSettings.ClientSecret)
+	assert.Equal(t, "some-account-name", archive.Settings.AzureBlobStorageSettings.AccountName)
+	assert.Equal(t, "some-container-name", archive.Settings.AzureBlobStorageSettings.ContainerName)
+	assert.Empty(t, archive.Settings.AmazonS3StorageSettings)
+}
+
+func TestArchiveLogs_RetrieveArchiveApiFail(t *testing.T) {
+	underTest, teardown, err := setupArchiveLogsTest()
+	defer teardown()
+	assert.NoError(t, err)
+	archiveId := int32(1234)
+
+	mux.HandleFunc(archiveApiBasePath+"/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Contains(t, r.URL.String(), strconv.FormatInt(int64(archiveId), 10))
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+
+	archive, err := underTest.RetrieveArchiveLogsSetting(archiveId)
+	assert.Error(t, err)
+	assert.Nil(t, archive)
+}
+
+func TestArchiveLogs_RetrieveArchiveIdNotFound(t *testing.T) {
+	underTest, teardown, err := setupArchiveLogsTest()
+	defer teardown()
+	assert.NoError(t, err)
+	archiveId := int32(1234)
+
+	mux.HandleFunc(archiveApiBasePath+"/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method)
+		assert.Contains(t, r.URL.String(), strconv.FormatInt(int64(archiveId), 10))
+		w.WriteHeader(http.StatusNotFound)
+	})
+
+	archive, err := underTest.RetrieveArchiveLogsSetting(archiveId)
+	assert.Error(t, err)
+	assert.Nil(t, archive)
+	assert.Contains(t, err.Error(), "failed with missing archive")
+}
