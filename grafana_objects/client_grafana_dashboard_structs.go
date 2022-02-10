7c94ef8d733a5d7d183c2d59ad37cd5a3156cb24
Etienne Journet <etienne.journet@non.se.com>
2022-02-10 18:09:20 +0100

    modified wrong objects or field, changed struct to pointer to stuct to allow for null fields in json

--- grafana_objects/client_grafana_dashboard_structs.go
+++ grafana_objects/client_grafana_dashboard_structs.go
@@ -9,22 +9,25 @@ type DashboardMeta struct {
 }
 
 type DashboardObject struct {
-	Id            int                      `json:"id"`
-	Uid           string                   `json:"uid"`
-	Title         string                   `json:"title"`
-	Tags          []string                 `json:"tags"`
-	Style         string                   `json:"style"`
-	Timezone      string                   `json:"timezone"`
-	Editable      bool                     `json:"editable"`
-	GraphToolTip  int                      `json:"graphTooltip"`
-	Time          TimeRange                `json:"time"`
-	Timepicker    Timepicker               `json:"timepicker"`
-	Templating    TemplatingList           `json:"templating"`
-	Annotations   interface{}              `json:"annotations"`
-	Refresh       string                   `json:"refresh"`
-	SchemaVersion int                      `json:"schemaVersion"`
-	Version       int                      `json:"version"`
-	Panels        []map[string]interface{} `json:"panels"`
+	Id            int            `json:"id,omitempty"`
+	Uid           string         `json:"uid"`
+	GnetId        *string        `json:"gnetId"`
+	Title         string         `json:"title"`
+	Tags          []string       `json:"tags"`
+	Style         string         `json:"style"`
+	Iteration     int            `json:"iteration"`
+	Links         []string       `json:"links"`
+	Timezone      string         `json:"timezone"`
+	Editable      bool           `json:"editable"`
+	GraphToolTip  int            `json:"graphTooltip"`
+	Time          TimeRange      `json:"time"`
+	Timepicker    *Timepicker    `json:"timepicker,omitempty"`
+	Templating    TemplatingList `json:"templating"`
+	Annotations   interface{}    `json:"annotations"`
+	Refresh       string         `json:"refresh"`
+	SchemaVersion int            `json:"schemaVersion"`
+	Version       int            `json:"version,omitempty"`
+	Panels        []interface{}  `json:"panels"`
 }
 
 type TimeRange struct {
@@ -33,13 +36,13 @@ type TimeRange struct {
 }
 
 type Timepicker struct {
-	Collapse         bool     `json:"collapse"`
-	Enable           bool     `json:"enable"`
-	Notice           bool     `json:"notice"`
-	Now              bool     `json:"now"`
-	RefreshIntervals []string `json:"refresh_intervals"`
-	Status           string   `json:"status"`
-	Type             string   `json:"type"`
+	Collapse         bool     `json:"collapse,omitempty"`
+	Enable           bool     `json:"enable,omitempty"`
+	Notice           bool     `json:"notice,omitempty"`
+	Now              bool     `json:"now,omitempty"`
+	RefreshIntervals []string `json:"refresh_intervals,omitempty"`
+	Status           string   `json:"status,omitempty"`
+	Type             string   `json:"type,omitempty"`
 }
 
 type TemplatingList struct {
@@ -48,19 +51,35 @@ type TemplatingList struct {
 }
 
 type TemplatingItem struct {
-	AllFormat   string                 `json:"string"`
+	AllFormat   *string                `json:"string"`
 	Current     map[string]interface{} `json:"current"`
-	Datasource  string                 `json:"datasource"`
+	Datasource  *string                `json:"datasource"`
+	Definition  *string                `json:"definition"`
+	Description *string                `json:"description"`
 	IncludeAll  bool                   `json:"includeAll"`
+	AllValue    *string                `json:"allValue"`
 	Multi       bool                   `json:"multi"`
-	MultiFormat string                 `json:"multiFormat"`
-	Name        string                 `json:"name"`
+	Name        *string                `json:"name"`
 	Options     []TemplatingOptions    `json:"options"`
-	Type        string                 `json:"type"`
+	Type        *string                `json:"type"`
+	Query       Query                  `json:"query"`
+	Error       *string                `json:"error"`
+	Hide        int                    `json:"hide"`
+	Label       *string                `json:"label"`
+	Refresh     int                    `json:"refresh"`
+	Regex       *string                `json:"regex"`
+	SkipUrlSync bool                   `json:"skipUrlSync"`
+	Sort        int                    `json:"sort"`
+	String      *string                `json:"string"`
 }
 
 type TemplatingOptions struct {
 	Selected bool   `json:"selected"`
-	Text     string `json:"test"`
+	Text     string `json:"text"`
 	Value    string `json:"value"`
+}
+
+type Query struct {
+	Query string `json:"query"`
+	RefId string `json:"refId"`
 }
