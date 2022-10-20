c7d8ca41cd3a45688949c065f7fe64e328ce35ae
Miri Bar <37478344+mirii1994@users.noreply.github.com>
2022-10-20 13:57:49 +0300

    Fix ReservedDailyGb bug (#103)
    * reservedDailyGb -> *float32, readme changelog
    
    * update workflow git-secrets

--- sub_accounts/client_sub_account.go
+++ sub_accounts/client_sub_account.go
@@ -29,7 +29,7 @@ type CreateOrUpdateSubAccount struct {
 	Email                  string                                   `json:"email,omitempty"`
 	AccountName            string                                   `json:"accountName"`
 	Flexible               string                                   `json:"isFlexible,omitempty"` // boolean
-	ReservedDailyGB        float32                                  `json:"reservedDailyGB,omitempty"`
+	ReservedDailyGB        *float32                                 `json:"reservedDailyGB,omitempty"`
 	MaxDailyGB             *float32                                 `json:"maxDailyGB,omitempty"`
 	RetentionDays          int32                                    `json:"retentionDays"`
 	Searchable             string                                   `json:"searchable,omitempty"` // boolean
