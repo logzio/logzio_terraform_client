a8f16a4c9c31904b252edacc42241e2de82ee1c7
mirii1994 <miri.ignatiev@logz.io>
2023-01-10 15:16:07 +0200

    client, create, get

---
+++ s3_buckets_connector/client_s3_buckets_connector.go
@@ -0,0 +1,90 @@
+package s3_buckets_connector
+
+import (
+	"fmt"
+	"github.com/logzio/logzio_terraform_client/client"
+)
+
+const (
+	s3BucketsServiceEndpoint         string = "%s/v1/log-shipping/s3-buckets"
+	createS3BucketConnectorOperation        = "CreateS3BucketConnector"
+	deleteDropFilterOperation               = "DeleteDropFilter"
+	retrieveDropFiltersOperation            = "RetrieveDropFilters"
+
+	s3BucketConnectorResourceName = "s3 bucket connector"
+
+	operationCreateS3BucketConnector = "CreateS3BucketConnector"
+	operationGetS3BucketConnector    = "GetS3BucketConnector"
+)
+
+type S3BucketsConnectorClient struct {
+	*client.Client
+}
+
+type S3BucketConnector struct {
+	AccessKey                string      `json:"accessKey,omitempty"`
+	SecretKey                string      `json:"secretKey,omitempty"`
+	Arn                      string      `json:"arn,omitempty"`
+	Bucket                   string      `json:"bucket"`
+	Prefix                   string      `json:"prefix,omitempty"`
+	Active                   *bool       `json:"active,omitempty"`
+	AddS3ObjectKeyAsLogField *bool       `json:"addS3ObjectKeyAsLogField,omitempty"`
+	Region                   AwsRegion   `json:"region"`
+	LogsType                 AwsLogsType `json:"logsType"`
+}
+
+type AwsRegion string
+type AwsLogsType string
+
+const (
+	RegionUsEast1      AwsRegion = "US_EAST_1"
+	RegionUsEast2      AwsRegion = "US_EAST_2"
+	RegionUsWest1      AwsRegion = "US_WEST_1"
+	RegionUsWest2      AwsRegion = "US_WEST_2"
+	RegionEuWest1      AwsRegion = "EU_WEST_1"
+	RegionEuWest2      AwsRegion = "EU_WEST_2"
+	RegionEuWest3      AwsRegion = "EU_WEST_3"
+	RegionEuCentral1   AwsRegion = "EU_CENTRAL_1"
+	RegionApNortheast1 AwsRegion = "AP_NORTHEAST_1"
+	RegionApNortheast2 AwsRegion = "AP_NORTHEAST_2"
+	RegionApSoutheast1 AwsRegion = "AP_SOUTHEAST_1"
+	RegionApSoutheast2 AwsRegion = "AP_SOUTHEAST_2"
+	RegionSaEast1      AwsRegion = "SA_EAST_1"
+	RegionApSouth1     AwsRegion = "AP_SOUTH_1"
+	RegionCaCentral1   AwsRegion = "CA_CENTRAL_1"
+
+	LogsTypeElb        AwsLogsType = "elb"
+	LogsTypeVpcFlow    AwsLogsType = "vpcflow"
+	LogsTypeS3Access   AwsLogsType = "S3Access"
+	LogsTypeCloudfront AwsLogsType = "cloudfront"
+)
+
+func (r AwsRegion) String() string {
+	return string(r)
+}
+
+func (t AwsLogsType) String() string {
+	return string(t)
+}
+
+func New(apiToken, baseUrl string) (*S3BucketsConnectorClient, error) {
+	if len(apiToken) == 0 {
+		return nil, fmt.Errorf("API token not defined")
+	}
+	if len(baseUrl) == 0 {
+		return nil, fmt.Errorf("Base URL not defined")
+	}
+	c := &S3BucketsConnectorClient{
+		Client: client.New(apiToken, baseUrl),
+	}
+
+	return c, nil
+}
+
+func validateCreateS3BucketRequest(req S3BucketConnector) error {
+	if len(req.Bucket) == 0 {
+		return fmt.Errorf("field bucket must be set")
+	}
+
+	return nil
+}
---
+++ s3_buckets_connector/client_s3_buckets_connector_create.go
@@ -0,0 +1,52 @@
+package s3_buckets_connector
+
+import (
+	"encoding/json"
+	"fmt"
+	logzio_client "github.com/logzio/logzio_terraform_client"
+	"net/http"
+)
+
+const (
+	createS3BucketsConnectorServiceUrl     = s3BucketsServiceEndpoint
+	createS3BucketsConnectorServiceMethod  = http.MethodPost
+	createS3BucketsConnectorMethodCreated  = http.StatusCreated
+	createS3BucketsConnectorStatusNotFound = http.StatusNotFound
+)
+
+// CreateS3BucketConnector creates a new S3 bucket connector if successful, an error otherwise
+func (c *S3BucketsConnectorClient) CreateS3BucketConnector(req S3BucketConnector) (*S3BucketConnector, error) {
+	err := validateCreateS3BucketRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	createS3ConnectorJson, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := logzio_client.CallLogzioApi(logzio_client.LogzioApiCallDetails{
+		ApiToken:     c.ApiToken,
+		HttpMethod:   createS3BucketsConnectorServiceMethod,
+		Url:          fmt.Sprintf(createS3BucketsConnectorServiceUrl, c.BaseUrl),
+		Body:         createS3ConnectorJson,
+		SuccessCodes: []int{createS3BucketsConnectorMethodCreated},
+		NotFoundCode: createS3BucketsConnectorStatusNotFound,
+		ResourceId:   nil,
+		ApiAction:    operationCreateS3BucketConnector,
+		ResourceName: s3BucketConnectorResourceName,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var retVal S3BucketConnector
+	err = json.Unmarshal(res, &retVal)
+	if err != nil {
+		return nil, err
+	}
+
+	return &retVal, nil
+}
