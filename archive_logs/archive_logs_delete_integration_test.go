9fc75f7f19fb9fb778e659d076970605e4b15c29
mirii1994 <miri.ignatiev@logz.io>
2023-02-14 12:29:24 +0200

    remove defer

--- archive_logs/archive_logs_delete_integration_test.go
+++ archive_logs/archive_logs_delete_integration_test.go
@@ -10,7 +10,6 @@ import (
 
 func TestIntegrationArchiveLogs_DeleteArchive(t *testing.T) {
 	underTest, err := setupArchiveLogsIntegrationTest()
-	defer test_utils.TestDoneTimeBuffer()
 
 	if assert.NoError(t, err) {
 		createArchive, err := test_utils.GetCreateOrUpdateArchiveLogs(archive_logs.StorageTypeS3)
