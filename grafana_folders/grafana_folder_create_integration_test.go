4ad63505b482ec7ab41d017db7ff5d1971dc771a
mirii1994 <miri.ignatiev@logz.io>
2023-05-07 15:52:03 +0300

    create tests

---
+++ grafana_folders/grafana_folder_create_integration_test.go
@@ -0,0 +1,40 @@
+package grafana_folders_test
+
+import (
+	"fmt"
+	"github.com/logzio/logzio_terraform_client/test_utils"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestIntegrationGrafanaFolder_CreateGrafanaFolder(t *testing.T) {
+	underTest, err := setupGrafanaFolderIntegrationTest()
+	defer test_utils.TestDoneTimeBuffer()
+
+	if assert.NoError(t, err) {
+		createGrafanaFolder := getCreateOrUpdateGrafanaFolder()
+		createGrafanaFolder.Title = fmt.Sprintf("%s_%s", createGrafanaFolder.Title, "create")
+		grafanaFolder, err := underTest.CreateGrafanaFolder(createGrafanaFolder)
+		if assert.NoError(t, err) && assert.NotNil(t, grafanaFolder) {
+			time.Sleep(4 * time.Second)
+			defer underTest.DeleteGrafanaFolder(grafanaFolder.Uid)
+			assert.Equal(t, createGrafanaFolder.Uid, grafanaFolder.Uid)
+			assert.Equal(t, createGrafanaFolder.Title, grafanaFolder.Title)
+			assert.Equal(t, int64(1), grafanaFolder.Version)
+		}
+	}
+}
+
+func TestIntegrationGrafanaFolder_CreateGrafanaFolderNoTitle(t *testing.T) {
+	underTest, err := setupGrafanaFolderIntegrationTest()
+	defer test_utils.TestDoneTimeBuffer()
+
+	if assert.NoError(t, err) {
+		createGrafanaFolder := getCreateOrUpdateGrafanaFolder()
+		createGrafanaFolder.Title = ""
+		grafanaFolder, err := underTest.CreateGrafanaFolder(createGrafanaFolder)
+		assert.Error(t, err)
+		assert.Nil(t, grafanaFolder)
+	}
+}
---
+++ grafana_folders/grafana_folder_create_test.go
@@ -0,0 +1,65 @@
+package grafana_folders_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/logzio/logzio_terraform_client/grafana_folders"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func TestGrafanaFolder_CreateGrafanaFolder(t *testing.T) {
+	underTest, err, teardown := setupGrafanaFolderTest()
+	defer teardown()
+
+	if assert.NoError(t, err) {
+		mux.HandleFunc("/v1/grafana/api/folders", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			jsonBytes, _ := io.ReadAll(r.Body)
+			var target grafana_folders.CreateUpdateFolder
+			err = json.Unmarshal(jsonBytes, &target)
+			assert.NoError(t, err)
+			assert.NotNil(t, target)
+			assert.NotEmpty(t, target.Title)
+			assert.NotEmpty(t, target.Uid)
+			w.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(w, fixture("create_grafana_folder_res.json"))
+		})
+
+		createGrafanaFolder := getCreateOrUpdateGrafanaFolder()
+		grafanaFolder, err := underTest.CreateGrafanaFolder(createGrafanaFolder)
+		assert.NoError(t, err)
+		assert.NotNil(t, grafanaFolder)
+		assert.Equal(t, int64(123456), grafanaFolder.Id)
+		assert.Equal(t, createGrafanaFolder.Uid, grafanaFolder.Uid)
+		assert.Equal(t, createGrafanaFolder.Title, grafanaFolder.Title)
+		assert.Equal(t, "/grafana-app/dashboards/f/client_test/client_test", grafanaFolder.Url)
+		assert.Equal(t, int64(1), grafanaFolder.Version)
+	}
+}
+
+func TestGrafanaFolder_CreateGrafanaFolderInternalServerError(t *testing.T) {
+	underTest, err, teardown := setupGrafanaFolderTest()
+	defer teardown()
+
+	if assert.NoError(t, err) {
+		mux.HandleFunc("/v1/grafana/api/folders", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, http.MethodPost, r.Method)
+			jsonBytes, _ := io.ReadAll(r.Body)
+			var target grafana_folders.CreateUpdateFolder
+			err = json.Unmarshal(jsonBytes, &target)
+			assert.NoError(t, err)
+			assert.NotNil(t, target)
+			assert.NotEmpty(t, target.Title)
+			assert.NotEmpty(t, target.Uid)
+			w.WriteHeader(http.StatusInternalServerError)
+		})
+
+		createGrafanaFolder := getCreateOrUpdateGrafanaFolder()
+		grafanaFolder, err := underTest.CreateGrafanaFolder(createGrafanaFolder)
+		assert.Error(t, err)
+		assert.Nil(t, grafanaFolder)
+	}
+}
