6e9eebb78c379d9c97ed5a95490b26b9de3b3298
mirii1994 <miri.ignatiev@logz.io>
2023-05-07 17:22:48 +0300

    fix update test

--- grafana_folders/grafana_folder_update_test.go
+++ grafana_folders/grafana_folder_update_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestS3Fetcher_UpdateFetcher(t *testing.T) {
+func TestGrafanaFolder_UpdateGrafanaFolder(t *testing.T) {
 	underTest, err, teardown := setupGrafanaFolderTest()
 	assert.NoError(t, err)
 	defer teardown()
@@ -32,4 +32,30 @@ func TestS3Fetcher_UpdateFetcher(t *testing.T) {
 	updateFolder := getCreateOrUpdateGrafanaFolder()
 	err = underTest.UpdateGrafanaFolder(updateFolder)
 	assert.NoError(t, err)
+}
+
+func TestGrafanaFolder_UpdateGrafanaFolderInternalServerError(t *testing.T) {
+	underTest, err, teardown := setupGrafanaFolderTest()
+	assert.NoError(t, err)
+	defer teardown()
+
+	folderId := "client_test"
+
+	mux.HandleFunc("/v1/grafana/api/folders/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPut, r.Method)
+		assert.Contains(t, r.URL.String(), folderId)
+		jsonBytes, _ := io.ReadAll(r.Body)
+		var target grafana_folders.CreateUpdateFolder
+		err = json.Unmarshal(jsonBytes, &target)
+		assert.NoError(t, err)
+		assert.NotNil(t, target)
+		assert.NotEmpty(t, target.Title)
+		assert.Equal(t, folderId, target.Uid)
+		assert.True(t, target.Overwrite)
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+
+	updateFolder := getCreateOrUpdateGrafanaFolder()
+	err = underTest.UpdateGrafanaFolder(updateFolder)
+	assert.Error(t, err)
 }
