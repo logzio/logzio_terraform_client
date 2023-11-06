3eda9055238f9cfd124ff42b54c35433b20f78b6
mirii1994 <miri.ignatiev@logz.io>
2023-11-06 13:56:55 +0200

    test

--- grafana_datasources/client_grafana_datasource.go
+++ grafana_datasources/client_grafana_datasource.go
@@ -18,23 +18,23 @@ type GrafanaDatasourceClient struct {
 }
 
 type GrafanaDataSource struct {
-	ID              int64                  `json:"id,omitempty"`
-	UID             string                 `json:"uid,omitempty"`
+	Id              int64                  `json:"id,omitempty"`
+	Uid             string                 `json:"uid,omitempty"`
 	Name            string                 `json:"name"`
 	Type            string                 `json:"type"`
-	TypeLogoURL     string                 `json:"typeLogoUrl,omitempty"` // Only returned by the API. Depends on the Type.
-	URL             string                 `json:"url"`
+	TypeLogoUrl     string                 `json:"typeLogoUrl,omitempty"` // Only returned by the API. Depends on the Type.
+	Url             string                 `json:"url"`
 	Access          string                 `json:"access"`
 	ReadOnly        bool                   `json:"readOnly"` // Only returned by the API. Can be set through the `editable` attribute of provisioned data sources.
 	Database        string                 `json:"database,omitempty"`
 	User            string                 `json:"user,omitempty"`
-	OrgID           int64                  `json:"orgId,omitempty"`
+	OrgId           int64                  `json:"orgId,omitempty"`
 	IsDefault       bool                   `json:"isDefault"`
 	BasicAuth       bool                   `json:"basicAuth"`
 	BasicAuthUser   string                 `json:"basicAuthUser,omitempty"`
 	WithCredentials bool                   `json:"withCredentials,omitempty"`
-	JSONData        map[string]interface{} `json:"jsonData,omitempty"`
-	SecureJSONData  map[string]interface{} `json:"secureJsonData,omitempty"`
+	JsonData        map[string]interface{} `json:"jsonData,omitempty"`
+	SecureJsonData  map[string]interface{} `json:"secureJsonData,omitempty"`
 	Version         int                    `json:"version,omitempty"`
 }
 
